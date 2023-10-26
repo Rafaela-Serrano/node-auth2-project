5e16d31a1d30b6ddc557e1b1ab568178a95e001b
Rafaela Serrano <raphaela.alonsoserrano@gmail.com>
2023-10-26 14:52:31 -0500

    create a find model function

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,23 +1,44 @@
 const db = require('../../data/db-config.js');
 
 function find() {
-  /**
-    You will need to join two tables.
-    Resolves to an ARRAY with all users.
+/** 
+select 
+u.user_id,
+u.username,
+r.role_name
+from users as u
+join roles as r 
+on u.role_id = r.role_id;
 
-    [
-      {
-        "user_id": 1,
-        "username": "bob",
-        "role_name": "admin"
-      },
-      {
-        "user_id": 2,
-        "username": "sue",
-        "role_name": "instructor"
-      }
-    ]
+You will need to join two tables.
+Resolves to an ARRAY with all users.
+
+[
+  {
+  "user_id": 1,
+  "username": "bob",
+  "role_name": "admin"
+  },
+  {
+  "user_id": 2,
+  "username": "sue",
+  "role_name": "instructor"
+  }
+]
    */
+
+return db( 'users as u' )
+  .join(
+    'roles as r',
+    'u.role_id',
+    'r.role_name'
+    )
+  .select(
+    'u.user_id',
+    'u.username',
+    'r.role_name'
+    )
+   
 }
 
 function findBy(filter) {
